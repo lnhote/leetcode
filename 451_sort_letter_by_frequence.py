2f7877a054ea2e2b873b47c7c2232467f465696a
林浩特 <lnhote@gmail.com>
2017-02-11 14:33:40 +0800

    improve

--- 451_sort_letter_by_frequence.py
+++ 451_sort_letter_by_frequence.py
@@ -3,8 +3,26 @@ class Element(object):
         self.ch = ch
         self.count = count
 
+
 class Solution(object):
     def frequencySort(self, s):
+        freq = {}
+        for i, ch in enumerate(s):
+            if ch not in freq:
+                freq[ch] = 0
+            freq[ch] = freq[ch]+1
+        bucket = ['']*(len(s)+1)
+        for i, key in enumerate(freq):
+            val = freq[key]
+            if val > 0:
+                bucket[val] = bucket[val] + key*val
+        result = ''
+        for i in range(len(bucket)-1, -1, -1):
+            if bucket[i] != '':
+                result = result + bucket[i]
+        return result
+
+    def frequencySort1(self, s):
         """
         :type s: str
         :rtype: str
@@ -17,7 +35,7 @@ class Solution(object):
             table[index] = table[index] + 1
 
         elements = []
-        for i in range(0,128):
+        for i in range(0, 128):
             letter_count = table[i]
             if letter_count > 0:
                 ch = chr(i)
@@ -26,11 +44,9 @@ class Solution(object):
 
         self.quicksort(elements, 0, len(elements)-1)
         result = ''
-        for i in range(len(elements)-1,-1,-1):
+        for i in range(len(elements)-1, -1, -1):
             result = result + elements[i].ch*elements[i].count
         return result
-
-
 
     def partion(self, elements, start, end):
         # print 'partion ', [ele.count for ele in elements], start, end
@@ -55,4 +71,6 @@ class Solution(object):
             self.quicksort(elements, mid+1, end)
 
 if __name__ == '__main__':
+    print Solution().frequencySort('')
     print Solution().frequencySort('tree')
+    print Solution().frequencySort('eeee')
