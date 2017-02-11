e5bc89e81827cda4083233b79e3a4e67736c605e
Hunter Lin <lnhote@gmail.com>
2017-02-11 17:32:53 +0800

    add new one

--- 128_longest_consecutive_sequence.py
+++ 128_longest_consecutive_sequence.py
@@ -1,34 +1,35 @@
 class Solution(object):
-    def longestConsecutive(self, nums):
+    def isValid(self, s):
         """
-        :type nums: List[int]
-        :rtype: int
+        :type s: str
+        :rtype: bool
         """
-        num_map = {}
-        max_len = 1
-        if len(nums) == 0:
-            return 0
-        for i, num in enumerate(nums):
-            if num not in num_map:
-                num_map[num] = 1
-                
-                length = 1
-                left = right = num
-                
-                if num-1 in num_map:
-                    length = length + num_map[num-1]
-                    left = num - num_map[num-1]
-                if num+1 in num_map:
-                    length = length + num_map[num+1]
-                    right = num + num_map[num+1]
-                num_map[left] = length
-                num_map[right] = length
-                if length > max_len:
-                    max_len = length
+        stack = []
+        if len(s) == 0:
+            return False
+        for i in range(0, len(s)):
+            if s[i] in ['(','[', '{']:
+                stack.append(s[i])
             else:
-                pass
-        return max_len
+                top = ''
+                if len(stack) > 0:
+                    top = stack.pop()
+                if top == '(' and s[i] == ')':
+                    continue
+                if top == '[' and s[i] == ']':
+                    continue
+                if top == '{' and s[i] == '}':
+                    continue
+                return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
                 
 
 if __name__ == '__main__':
-	print Solution().longestConsecutive([100, 4, 200, 1, 3, 2,5]) # 5
+	print Solution().isValid('()') # True
+	print Solution().isValid('()[]{}') # True
+	print Solution().isValid('(]') # False
+	print Solution().isValid('([)]') # False
+	print Solution().isValid('') # False
